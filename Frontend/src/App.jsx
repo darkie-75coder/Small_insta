454fad8804e6b457f74cad006801c143e0f66f65
darkie-75coder <shivanshkumargupta75@gmail.com>
2026-03-17 11:42:31 +0530

    Replace Feed component with Home on root route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import CreatePost from "./components/CreatePost";
 import Feed from "./components/Feed";
+import Home from "./components/Home";
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/create-post" element={<CreatePost />} />
         <Route path="/feed" element={<Feed />} />
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
