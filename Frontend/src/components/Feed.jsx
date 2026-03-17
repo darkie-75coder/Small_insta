03d27629ecb2ee16512aadcb3ee6808030a3db03
darkie-75coder <shivanshkumargupta75@gmail.com>
2026-03-17 11:41:50 +0530

    Update Feed.jsx

--- Frontend/src/components/Feed.jsx
+++ Frontend/src/components/Feed.jsx
@@ -1,58 +1,33 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
-const CreatePost = () => {
-  const navigate = useNavigate();
+const Feed = () => {
+  const [posts, setPosts] = useState([]);
 
-  const [image, setImage] = useState(null);
-  const [caption, setCaption] = useState("");
-
-  async function Handler(e) {
-    e.preventDefault();
-    const formData = new FormData();
-
-    formData.append("image", image);
-    formData.append("caption", caption);
-
-    navigate("/feed");
-
-    try {
-      await axios.post(
-        "https://small-insta.onrender.com/create-post",
-        formData,
-      );
-    } catch (err) {
-      console.log(err);
+  useEffect(() => {
+    async function getPosts() {
+      const res = await axios.get("https://small-insta.onrender.com/feed");
+      setPosts(res.data.posts);
     }
-  }
+
+    getPosts();
+  }, []);
 
   return (
-    <div className="create-box">
-      <form onSubmit={Handler}>
-        <div className="text">
-          <h1>Create Post</h1>
-        </div>
-        <input
-          type="file"
-          required
-          onChange={(e) => {
-            setImage(e.target.files[0]);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Enter Caption"
-          required
-          onChange={(e) => {
-            setCaption(e.target.value);
-          }}
-        />
-
-        <button type="submit">Submit</button>
-      </form>
+    <div className="feed-box">
+      <div className="text-box">
+        <h1>Your Posts</h1>
+      </div>
+      {posts.map((post) => {
+        return (
+          <div className="post" key={post._id}>
+            <img src={post.image} alt="image" />
+            <p>{post.caption}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-export default CreatePost;
+export default Feed;
